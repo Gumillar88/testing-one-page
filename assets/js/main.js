29297d562c1bc6f06f935b8c9f5316e300e9d31e
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2016-07-24 15:44:16 +0700

    new upload file
    new upload file

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $('#send').click(function(){
+        alert('this is alert Send Submit');
+    });
+
+    $('#cancel').click(function(){
+        alert('this is alert Cancel Submit');
+    });
+});
